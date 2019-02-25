66cd0c295f54d501b0b9ff51029979aba9ac188b
JH <jh_p0415@naver.com>
2019-02-25 17:59:04 +0900

    2019.02.25
    2019.02.25_1

---
+++ Android/PlacesAPI01/app/src/main/java/com/example/jhp0415/placesapi01/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.jhp0415.placesapi01;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1=(Button)findViewById(R.id.current_btn);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CurrentPlaceTestActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button2=(Button)findViewById(R.id.Search_btn);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PlaceSearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+
+
+}
