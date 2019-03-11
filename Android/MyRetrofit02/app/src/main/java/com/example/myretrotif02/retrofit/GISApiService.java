0f80d2f41807bd6ce817730312043395b4ffa182
JH <jh_p0415@naver.com>
2019-03-11 17:28:49 +0900

    2019.03.11-2

--- Android/MyRetrofit02/app/src/main/java/com/example/myretrotif02/retrofit/GISApiService.java
+++ Android/MyRetrofit02/app/src/main/java/com/example/myretrotif02/retrofit/GISApiService.java
@@ -14,16 +14,6 @@ import retrofit2.http.QueryMap;
 public interface GISApiService {
     final String BASE_URL = "https://gis.kt.com/";
 
-//    @Headers({"Authorization:Bearer eb142d9027f84d51a4a20df8490e44bcf6fc7ef4dea64cae96a7fca282ebd8cc02764651",
-//            "Accept: application/json"})
-//    @GET("search/v1.0/pois/{id}")
-//    Call<ResponseBody> retrievePOI(@Path("id") String id);
-//
-//    @Headers({"Authorization:Bearer eb142d9027f84d51a4a20df8490e44bcf6fc7ef4dea64cae96a7fca282ebd8cc02764651",
-//            "Accept: application/json"})
-//    @GET("search/v1.0/pois")
-//    Call<ResponseBody> poiSearch(@QueryMap HashMap<String, String> parameters);
-
     @Headers({"Authorization:Bearer eb142d9027f84d51a4a20df8490e44bcf6fc7ef4dea64cae96a7fca282ebd8cc02764651",
             "Accept: application/json"})
     @GET("search/v1.0/pois/{id}")
